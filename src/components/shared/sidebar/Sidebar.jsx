9c25e3388102ca397970417a88108724e7f831a6
Angelita Santos <angelitasantos@gmail.com>
2021-11-20 19:03:25 -0300

    create component sidebar

---
+++ src/components/shared/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ContainerSidebar } from '../../styles';
+
+const Sidebar = () => {
+    return (
+        <ContainerSidebar>
+            Sidebar
+        </ContainerSidebar>
+    );
+}
+
+export default Sidebar;
