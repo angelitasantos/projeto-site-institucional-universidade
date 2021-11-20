5133ad39192f4f92fe71d64ffa58c991ad6be0a1
Angelita Santos <angelitasantos@gmail.com>
2021-11-20 14:12:24 -0300

    install styled-reset dependence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
 import theme from './theme';
 import './App.css';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Reset />
       <div className="App">
         Learn React <i class="fas fa-home"></i>
       </div>
