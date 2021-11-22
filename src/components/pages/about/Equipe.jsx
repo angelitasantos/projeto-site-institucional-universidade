c791d4be8fb4108bf0cf93910a98f2dea35c3e81
Angelita Santos <angelitasantos@gmail.com>
2021-11-22 20:27:44 -0300

    create and organize component equipe

--- src/components/pages/about/Equipe.jsx
+++ src/components/pages/about/Equipe.jsx
@@ -8,13 +8,13 @@ const Equipe = () => {
         <div className="container-equipe display-flex flex-direction-row align-items-center justify-content-space-evenly text-align-center">
             <CardEquipe
                 nomeEquipe="Profissional A"
-                imagemEquipe="images/angelita.png"
+                imagemEquipe="images/profissionalA.png"
                 profissaoEquipe="Profissional A"
                 textoEquipe="XX anos de experiência sendo X anos nas áreas de Custos e Planejamento em indústrias e 
                 distribuidoras e X anos na área Financeira em lojas de auto peças." />
             <CardEquipe
                 nomeEquipe="Profissional B"
-                imagemEquipe="images/sylmara.png"
+                imagemEquipe="images/profissionalB.png"
                 profissaoEquipe="Profissional B"
                 textoEquipe="X anos de experiência na área de produção, sendo X anos de experiência na área gestão de 
                 processos e X anos de experiência em criação e criação de produto." />
