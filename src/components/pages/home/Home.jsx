8fc06d207c2cea86f355589b7c0d61957f41a44b
Angelita Santos <angelitasantos@gmail.com>
2021-11-28 12:09:14 -0300

    create and organize component slides

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import '../../../App.css';
+import './home.css';
+
+import { Container } from '../../styles';
 
 import Header from '../../shared/header/Header';
+import Slides from './Slides';
 import Footer from '../../shared/footer/Footer';
 
 const Home = () => (
     <div>
         <Header />
+        <Container className="container-home">
+            <Slides />
+        </Container>
         <Footer />
     </div>
 );
